1db5eee97ad8adba4a106d7a784c843eeb4f5af8
Tom Moroney <moroneyt@tcd.ie>
2024-10-06 03:22:12 +0100

    Removed learn more button

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -132,11 +132,6 @@ export default function MertyMGMTLanding() {
                         </Button>
                       </Link>
                     </div>
-                    <div className="mt-3 sm:mt-0 sm:ml-3">
-                      <Button variant="outline" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-purple-700 bg-purple-100 hover:bg-purple-200 dark:text-purple-100 dark:bg-purple-900 dark:hover:bg-purple-800 md:py-4 md:text-lg md:px-10">
-                        Learn more
-                      </Button>
-                    </div>
                   </div>
                 </div>
               </main>
